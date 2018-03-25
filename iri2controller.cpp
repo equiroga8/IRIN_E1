018b68d1262a572009fbdb8f292b759ba596b608
equiroga8 <36472815+equiroga8@users.noreply.github.com>
2018-03-25 14:05:43 +0200

    Añadidos 2 Behaviors
    Avoid Priority - Para no chocar con las paredes
    Navigate Priority - El robot recorre en forma de espira (Hay que modificarlo más)

--- iri2controller.cpp
+++ iri2controller.cpp
@@ -33,6 +33,13 @@ extern long int rngSeed;
 
 using namespace std;
 
+#define BEHAVIORS 2
+#define AVOID_PRIORITY 0
+#define NAVIGATE_PRIORITY 1
+#define SPEED 500
+
+#define PROXIMITY_THRESHOLD 0.3
+
 CIri2Controller::CIri2Controller (const char* pch_name, CEpuck* pc_epuck, int n_write_to_file) : CController (pch_name, pc_epuck)
 
 {
@@ -65,9 +72,18 @@ CIri2Controller::CIri2Controller (const char* pch_name, CEpuck* pc_epuck, int n_
 	m_seRedBattery = (CRedBatterySensor*) m_pcEpuck->GetSensor (SENSOR_RED_BATTERY);
 	/* Set encoder Sensor */
 	m_seEncoder = (CEncoderSensor*) m_pcEpuck->GetSensor (SENSOR_ENCODER);
-  m_seEncoder->InitEncoderSensor(m_pcEpuck);
+  	m_seEncoder->InitEncoderSensor(m_pcEpuck);
 	/* Set compass Sensor */
 	m_seCompass = (CCompassSensor*) m_pcEpuck->GetSensor (SENSOR_COMPASS);
+
+	changeAngle = 0.3;
+	divider = 1;
+
+	m_fActivationTable = new double* [BEHAVIORS];
+	for ( int i = 0 ; i < BEHAVIORS ; i++ )
+	{
+		m_fActivationTable[i] = new double[3];
+	}
 
 }
 
@@ -76,6 +92,11 @@ CIri2Controller::CIri2Controller (const char* pch_name, CEpuck* pc_epuck, int n_
 
 CIri2Controller::~CIri2Controller()
 {
+	for ( int i = 0 ; i < BEHAVIORS ; i++ )
+	{
+		delete [] m_fActivationTable;
+	}
+
 }
 
 
@@ -204,6 +225,8 @@ void CIri2Controller::SimulationStep(unsigned n_step_number, double f_time, doub
 	/* Fin: Incluir las ACCIONES/CONTROLADOR a implementar */
 
 
+
+
 	FILE* filePosition = fopen("outputFiles/robotPosition", "a");
 	fprintf(filePosition," %2.4f %2.4f %2.4f %2.4f\n",
 	f_time, m_pcEpuck->GetPosition().x,
@@ -213,10 +236,23 @@ void CIri2Controller::SimulationStep(unsigned n_step_number, double f_time, doub
 	
 	
 
+	
+	/* Move time to global variable, so it can be used by the bahaviors to write to files*/
+	m_fTime = f_time;
+
+	/* Execute the levels of competence */
+	ExecuteBehaviors();
+
+	/* Execute Coordinator */
+	Coordinator();
+
+	/* Set Speed to wheels */
+	m_acWheels->SetSpeed(m_fLeftSpeed, m_fRightSpeed);	
+
 	/* Fase 3: ACTUACIÓN */
 	/* Option 1: Speed between -1000, 1000*/ 
 
-	m_acWheels->SetSpeed(1000*(bluelight[0]+bluelight[4]),1000*(bluelight[1]+bluelight[3]));
+	//m_acWheels->SetSpeed(1000*(bluelight[0]+bluelight[4]),1000*(bluelight[1]+bluelight[3]));
 
 	/* Option 2: Speed between 0,1*/
 	//m_acWheels->SetOutput(0,0.5);
@@ -227,3 +263,151 @@ void CIri2Controller::SimulationStep(unsigned n_step_number, double f_time, doub
 /******************************************************************************/
 /******************************************************************************/
 
+void CIri2Controller::ExecuteBehaviors ( void )
+{
+	for ( int i = 0 ; i < BEHAVIORS ; i++ )
+	{
+		m_fActivationTable[i][2] = 0.0;
+	}
+
+	/* Release Inhibitors IMPORTANT ------------------------------------------------*/
+	
+
+	/* Set Leds to BLACK */
+	m_pcEpuck->SetAllColoredLeds(	LED_COLOR_BLACK);
+	
+	ObstacleAvoidance ( AVOID_PRIORITY );
+	Navigate ( NAVIGATE_PRIORITY );
+}
+
+
+void CIri2Controller::Coordinator ( void )
+{
+	int nBehavior;
+  double fAngle = 0.0;
+
+  int nActiveBehaviors = 0;
+  /* For every Behavior Activated, sum angles */
+	for ( nBehavior = 0 ; nBehavior < BEHAVIORS ; nBehavior++ )
+	{
+		if ( m_fActivationTable[nBehavior][2] == 1.0 )
+		{
+      fAngle += m_fActivationTable[nBehavior][0];
+      nActiveBehaviors++;
+		}
+	}
+  fAngle /= (double) nActiveBehaviors;
+	
+  /* Normalize fAngle */
+  while ( fAngle > M_PI ) fAngle -= 2 * M_PI;
+	while ( fAngle < -M_PI ) fAngle += 2 * M_PI;
+ 
+  /* Based on the angle, calc wheels movements */
+  double fCLinear = 1.0;
+  double fCAngular = 1.0;
+  double fC1 = SPEED / M_PI;
+
+  /* Calc Linear Speed */
+  double fVLinear = SPEED * fCLinear * ( cos ( fAngle / 2) );
+
+  /*Calc Angular Speed */
+  double fVAngular = fAngle;
+
+  m_fLeftSpeed  = fVLinear - fC1 * fVAngular;
+  m_fRightSpeed = fVLinear + fC1 * fVAngular;
+	if (m_nWriteToFile ) 
+	{
+		/* INIT: WRITE TO FILES */
+		/* Write coordinator ouputs */
+		FILE* fileOutput = fopen("outputFiles/coordinatorOutput", "a");
+		fprintf(fileOutput,"%2.4f %d %2.4f %2.4f \n", m_fTime, nBehavior, m_fLeftSpeed, m_fRightSpeed);
+		fclose(fileOutput);
+		/* END WRITE TO FILES */
+	}
+}
+
+void CIri2Controller::ObstacleAvoidance ( unsigned int un_priority )
+{
+
+	
+	/* Leer Sensores de Proximidad */
+	double* prox = m_seProx->GetSensorReading(m_pcEpuck);
+
+	double fMaxProx = 0.0;
+	const double* proxDirections = m_seProx->GetSensorDirections();
+
+	dVector2 vRepelent;
+	vRepelent.x = 0.0;
+	vRepelent.y = 0.0;
+
+	/* Calc vector Sum */
+	for ( int i = 0 ; i < m_seProx->GetNumberOfInputs() ; i ++ )
+	{
+		vRepelent.x += prox[i] * cos ( proxDirections[i] );
+		vRepelent.y += prox[i] * sin ( proxDirections[i] );
+
+		if ( prox[i] > fMaxProx )
+			fMaxProx = prox[i];
+	}
+	
+	/* Calc pointing angle */
+	float fRepelent = atan2(vRepelent.y, vRepelent.x);
+	/* Create repelent angle */
+	fRepelent -= M_PI;
+	/* Normalize angle */
+	while ( fRepelent > M_PI ) fRepelent -= 2 * M_PI;
+	while ( fRepelent < -M_PI ) fRepelent += 2 * M_PI;
+
+  m_fActivationTable[un_priority][0] = fRepelent;
+  m_fActivationTable[un_priority][1] = fMaxProx;
+
+	/* If above a threshold */
+	if ( fMaxProx > PROXIMITY_THRESHOLD )
+	{
+
+		divider = 1;
+		changeAngle = 0.4;
+		/* Set Leds to GREEN */
+		m_pcEpuck->SetAllColoredLeds(	LED_COLOR_GREEN);
+    /* Mark Behavior as active */
+    m_fActivationTable[un_priority][2] = 1.0;
+	}
+	
+	if (m_nWriteToFile ) 
+	{
+		/* INIT WRITE TO FILE */
+		/* Write level of competence ouputs */
+		FILE* fileOutput = fopen("outputFiles/avoidOutput", "a");
+		fprintf(fileOutput, "%2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f %2.4f ", m_fTime, prox[0], prox[1], prox[2], prox[3], prox[4], prox[5], prox[6], prox[7], fMaxProx, fRepelent);
+		fprintf(fileOutput, "%2.4f %2.4f %2.4f\n",m_fActivationTable[un_priority][2], m_fActivationTable[un_priority][0], m_fActivationTable[un_priority][1]);
+		fclose(fileOutput);
+		/* END WRITE TO FILE */
+	}
+	
+}
+
+
+void CIri2Controller::Navigate ( unsigned int un_priority )
+{
+  /* Direction Angle 0.0 and always active. We set its vector intensity to 0.5 if used */
+	
+	changeAngle -= 0.001/divider;
+	divider += 0.001;
+
+	m_fActivationTable[un_priority][0] = changeAngle;
+	m_fActivationTable[un_priority][1] = 0.5;
+	m_fActivationTable[un_priority][2] = 1.0;
+
+	printf("changeAngle => %2.4f\n",m_fActivationTable[un_priority][0]);
+
+	if (m_nWriteToFile ) 
+	{
+		/* INIT: WRITE TO FILES */
+		/* Write level of competence ouputs */
+		FILE* fileOutput = fopen("outputFiles/navigateOutput", "a");
+		fprintf(fileOutput,"%2.4f %2.4f %2.4f %2.4f \n", m_fTime, m_fActivationTable[un_priority][2], m_fActivationTable[un_priority][0], m_fActivationTable[un_priority][1]);
+		fclose(fileOutput);
+		/* END WRITE TO FILES */
+	}
+
+}
---
+++ iri2controller.h
@@ -0,0 +1,57 @@
+#ifndef IRI2CONTROLLER_H_
+#define IRI2CONTROLLER_H_
+
+
+/******************************************************************************/
+/******************************************************************************/
+
+#include "controller.h"
+
+/******************************************************************************/
+/******************************************************************************/
+
+class CIri2Controller : public CController
+{
+public:
+
+    CIri2Controller (const char* pch_name, CEpuck* pc_epuck, int n_write_to_file);
+    ~CIri2Controller();
+    void SimulationStep(unsigned n_step_number, double f_time, double f_step_interval);
+
+private:
+    	CEpuck* m_pcEpuck;
+    
+		CWheelsActuator* m_acWheels;
+    	CEpuckProximitySensor* m_seProx;
+		CRealLightSensor* m_seLight;
+		CRealBlueLightSensor* m_seBlueLight;
+		CRealRedLightSensor* m_seRedLight;
+		CContactSensor* m_seContact;
+		CGroundSensor* m_seGround;
+		CGroundMemorySensor* m_seGroundMemory;
+		CBatterySensor* m_seBattery;  
+		CBlueBatterySensor* m_seBlueBattery;  
+		CRedBatterySensor* m_seRedBattery;  
+		CEncoderSensor* m_seEncoder;  
+		CCompassSensor* m_seCompass;  
+
+    float 		m_fOrientation; 
+    dVector2 	m_vPosition;
+	double 		m_fLeftSpeed;
+	double 		m_fRightSpeed;
+	double**	m_fActivationTable;
+	int 		m_nWriteToFile;
+	double 		m_fTime;
+	double		changeAngle;
+	double 		divider;
+
+	/* Functions */
+
+	void ExecuteBehaviors ( void );
+	void Coordinator ( void );
+
+	void ObstacleAvoidance ( unsigned int un_priority );
+	void Navigate ( unsigned int un_priority );
+};
+
+#endif
